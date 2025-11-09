14c6dc9e55a690b644e2349a7bf5cd04fe4ca72c
necipulusoy <necipulusoyy@gmail.com>
2025-11-09 12:50:46 +0300

    micro-ci-lib-added

---
+++ vars/dockerBuildPush.groovy
@@ -0,0 +1,51 @@
+def call(Map params = [:]) {
+    pipeline {
+        agent {
+            kubernetes {
+                label 'k8s-agent-multi'
+                defaultContainer 'docker'
+            }
+        }
+
+        environment {
+            REGISTRY   = "my-nexus-repository-manager.nexus.svc.cluster.local:8082"
+            REPO_PATH  = "repository/nexusimagerepository"
+            SERVICE    = params.service ?: "demo"
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    sh '''
+                        echo "Checking out source..."
+                        rm -rf repo || true
+                        git clone -b main https://github.com/hepapi/poc-env-microservices-demo.git repo
+                        cd repo/src/${SERVICE}
+                        echo "Source ready"
+                    '''
+                }
+            }
+
+            stage('Login to Nexus') {
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'nexus-docker-creds', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                        sh '''
+                            echo "$PASS" | docker login "http://$REGISTRY" -u "$USER" --password-stdin
+                        '''
+                    }
+                }
+            }
+
+            stage('Build & Push Image') {
+                steps {
+                    sh '''
+                        cd repo/src/${SERVICE}
+                        IMAGE_TAG="1.0.${BUILD_NUMBER}"
+                        docker build -t "$REGISTRY/$REPO_PATH/${SERVICE}:$IMAGE_TAG" .
+                        docker push "$REGISTRY/$REPO_PATH/${SERVICE}:$IMAGE_TAG"
+                    '''
+                }
+            }
+        }
+    }
+}
---
+++ vars/helmPackagePush.groovy
@@ -0,0 +1,56 @@
+def call(Map params = [:]) {
+    pipeline {
+        agent {
+            kubernetes {
+                label 'k8s-agent-multi'
+                defaultContainer 'helm'
+            }
+        }
+
+        environment {
+            SERVICE  = params.service ?: "demo"
+            NEXUS_HELM_REPO = "http://my-nexus-repository-manager.nexus.svc.cluster.local:8081/repository/nexushelmrepository/"
+        }
+
+        stages {
+            stage('Clone Helm Base and App Values') {
+                steps {
+                    sh '''
+                        rm -rf helm-base-common app-values helm-cur-chart
+                        git clone https://github.com/hepapi/helm-base-common.git
+                        git clone https://github.com/hepapi/app-values.git
+                    '''
+                }
+            }
+
+            stage('Prepare Helm Chart') {
+                steps {
+                    sh '''
+                        mkdir -p helm-cur-chart/templates
+                        cp -r helm-base-common/v1.32/templates/* helm-cur-chart/templates/
+                        cp helm-base-common/v1.32/Chart.yaml helm-cur-chart/
+                        cp app-values/non-prod/dev/${SERVICE}-values.yaml helm-cur-chart/values.yaml
+                    '''
+                }
+            }
+
+            stage('Patch & Push Chart') {
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'nexus-docker-creds', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
+                        sh '''
+                            IMAGE_TAG="1.0.${BUILD_NUMBER}"
+                            sed -i "s/^  tag:.*/  tag: ${IMAGE_TAG}/" helm-cur-chart/values.yaml
+                            sed -i "s/^appVersion:.*/appVersion: ${IMAGE_TAG}/" helm-cur-chart/Chart.yaml
+                            sed -i "s/^version:.*/version: ${IMAGE_TAG}/" helm-cur-chart/Chart.yaml
+
+                            helm lint ./helm-cur-chart
+                            helm package ./helm-cur-chart
+                            FILENAME=$(ls -t ${SERVICE}-*.tgz | head -n 1)
+                            curl -u "$NEXUS_USER:$NEXUS_PASS" --upload-file "$FILENAME" "${NEXUS_HELM_REPO}"
+                        '''
+                    }
+                }
+            }
+        }
+    }
+}
