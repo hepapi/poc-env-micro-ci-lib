b3337340167a70b8faa3c0e4e4679b96af2c2966
Necip Ulusoy <113590193+necipulusoy@users.noreply.github.com>
2026-05-21 17:57:29 +0300

    Update Config.groovy

--- src/org/hepapi/microcilib/Config.groovy
+++ src/org/hepapi/microcilib/Config.groovy
@@ -1,8 +1,8 @@
 package org.hepapi.microcilib
 
 class Config {
-  static final String REGISTRY   = "nexus.hepapi.com:8082"
+  static final String REGISTRY   = "my-nexus-repository-manager.nexus.svc.cluster.local:8082"
   static final String IMAGE_REPO = "repository/nexusimagerepository"
-  static final String HELM_REPO  = "https://nexus.hepapi.com/repository/nexushelmrepository/"
-  static final String CREDS_ID   = "nexus-docker-creds"
+  static final String HELM_REPO  = "http://my-nexus-repository-manager.nexus.svc.cluster.local:8081/repository/nexushelmrepository/"
+  static final String CREDS_ID   = "nexus-docker-creds"   // Jenkins Credentials ID (user/pass)
 }
