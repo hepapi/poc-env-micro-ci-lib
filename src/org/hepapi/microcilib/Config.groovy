0660b1cfc534c9fb09f7886a160446fc0b9df29d
Necip Ulusoy <113590193+necipulusoy@users.noreply.github.com>
2026-05-21 17:55:31 +0300

    Update Config.groovy

--- src/org/hepapi/microcilib/Config.groovy
+++ src/org/hepapi/microcilib/Config.groovy
@@ -1,7 +1,7 @@
 package org.hepapi.microcilib
 
 class Config {
-  static final String REGISTRY   = "nexus.hepapi.com"
+  static final String REGISTRY   = "nexus.hepapi.com:8082"
   static final String IMAGE_REPO = "repository/nexusimagerepository"
   static final String HELM_REPO  = "https://nexus.hepapi.com/repository/nexushelmrepository/"
   static final String CREDS_ID   = "nexus-docker-creds"
